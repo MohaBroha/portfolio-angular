3cdac93fd49f66bac54f74e62b74c86e62a5f6dd
mohabroha@mail.de <mohabroha@mail.de>
2026-07-16 22:58:19 +0200

    feat(hero): add typewriter animation with translations

--- src/app/hero/hero.ts
+++ src/app/hero/hero.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
-import { HostListener } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnInit
+} from '@angular/core';
+
+import {
+  TranslateModule,
+  TranslateService
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-hero',
@@ -10,7 +17,91 @@ import { HostListener } from '@angular/core';
   templateUrl: './hero.html',
   styleUrls: ['./hero.scss'],
 })
-export class HeroComponent {
+
+export class HeroComponent implements OnInit {
+
+  constructor(
+    private translate: TranslateService
+  ) {}
+
+  intro = '';
+  name = '';
+  roleTop = '';
+  roleBottom = '';
+
+  private textIntro = '';
+  private textName = '';
+  private textRoleTop = '';
+  private textRoleBottom = '';
+
+ngOnInit() {
+
+  this.translate.get([
+    'hero.intro',
+    'hero.name',
+    'hero.roleTop',
+    'hero.roleBottom'
+  ]).subscribe(translations => {
+
+    this.textIntro = translations['hero.intro'];
+    this.textName = translations['hero.name'];
+    this.textRoleTop = translations['hero.roleTop'];
+    this.textRoleBottom = translations['hero.roleBottom'];
+
+    this.startTyping();
+
+  });
+
+}
+
+async startTyping() {
+
+  await this.typeText(
+    this.textIntro,
+    value => this.intro = value
+  );
+
+  await this.typeText(
+    this.textName,
+    value => this.name = value
+  );
+
+  await this.typeText(
+    this.textRoleTop,
+    value => this.roleTop = value
+  );
+
+  await this.typeText(
+    this.textRoleBottom,
+    value => this.roleBottom = value
+  );
+
+}
+
+typeText(
+  text: string,
+  update: (value: string) => void
+): Promise<void> {
+
+  return new Promise(resolve => {
+
+    let index = 0;
+
+    const timer = setInterval(() => {
+
+      update(text.substring(0, index + 1));
+
+      index++;
+
+      if (index >= text.length) {
+        clearInterval(timer);
+        resolve();
+      }
+
+    }, 100);
+
+  });
+}
 
 scrollY = 0;
 @HostListener('window:scroll')
